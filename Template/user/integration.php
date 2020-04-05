e3016688dbcad4e02eccde52aef4c81fad6bbad6
Siddharth Kaul <siddharth.kaul@rail.bombardier.com>
2020-04-06 05:03:19 +0530

    Added the icon as shown in #2.
    Img source has to be added in all the front end part. Icon size 16x16 added to the repository.

--- Template/user/integration.php
+++ Template/user/integration.php
@@ -1,12 +1,10 @@
-<h3>Outlook</h3>
+<h3><img src="<?= $this->url->dir() ?>plugins/Outlook/outlook-icon.png"/>Outlook</h3>
 <div class="panel">
     <?= $this->form->label(t('Webhook URL'), 'outlook_webhook_url') ?>
     <?= $this->form->text('outlook_webhook_url', $values) ?>
 
     <?= $this->form->label(t('Channel/Group/User (Optional)'), 'outlook_webhook_channel') ?>
     <?= $this->form->text('outlook_webhook_channel', $values, array(), array('placeholder="@username"')) ?>
-    <?= $this->form->checkboxes('select_event',$this->eventManager->getAll(),array())
-    ?>
     <p class="form-help"><a href="https://github.com/k10blogger/kanboard-plugin-outlook#configuration" target="_blank"><?= t('Help on Outlook integration') ?></a></p>
 
     <div class="form-actions">
